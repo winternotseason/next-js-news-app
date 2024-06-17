7d7d65c424daed8fbddca05627ba560363b2f0dd
winternotseason <winternotseason@naver.com>
2024-06-17 21:30:22 +0900

    병렬 라우팅 사용하기

---
+++ components/news-list.js
@@ -0,0 +1,16 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+
+export default function NewsList({ news }) {
+  return (
+    <ul className="news-list">
+      {DUMMY_NEWS.map((newsItem) => (
+        <li key={newsItem.id}>
+          <Link href={`/news/${newsItem.slug}`}>
+            <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+            <span>{newsItem.title}</span>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
